5ae798a9976f114bc8474de71c2d8d9dacaeaad7
zangston <winston.y.zhang@gmail.com>
2023-10-26 16:55:22 -0400

    Revise table attributes, remove redundant tables

--- SQL/table-setup.sql
+++ SQL/table-setup.sql
@@ -15,7 +15,7 @@ CREATE TABLE user (
 -- follows(username, artist_id)
 CREATE TABLE follows (
     username VARCHAR(255),
-    artist_id INT,
+    artist_id VARCHAR(255),
     FOREIGN KEY (username) REFERENCES user(username),
     FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
@@ -23,7 +23,7 @@ CREATE TABLE follows (
 -- likes(username, release_id)
 CREATE TABLE likes (
     username VARCHAR(255),
-    release_id INT,
+    release_id VARCHAR(255),
     FOREIGN KEY (username) REFERENCES user(username),
     FOREIGN KEY (release_id) REFERENCES releases(release_id)
 );
@@ -45,53 +45,54 @@ CREATE TABLE recommendations (
 );
 
 -- owns(artist, song_title, album_title)
-CREATE TABLE owns (
-    artist VARCHAR(255),
-    song_title VARCHAR(255),
-    album_title VARCHAR(255),
-    FOREIGN KEY (artist) REFERENCES artist(artist_id),
-    FOREIGN KEY (song_title) REFERENCES song(song_title),
-    FOREIGN KEY (album_title) REFERENCES album(album_title)
-);
+-- CREATE TABLE owns (
+--     artist VARCHAR(255),
+--     song_title VARCHAR(255),
+--     album_title VARCHAR(255),
+--     FOREIGN KEY (artist) REFERENCES artist(artist_id),
+--     FOREIGN KEY (song_title) REFERENCES song(song_title),
+--     FOREIGN KEY (album_title) REFERENCES album(album_title)
+-- );
 
 -- song(song_id, artist, song_title, year, streams)
 CREATE TABLE song (
-    song_id INT PRIMARY KEY,
+    song_id VARCHAR(255) PRIMARY KEY,
     artist VARCHAR(255),
     song_title VARCHAR(255),
     year INT,
     streams INT,
+    duration INT,
     FOREIGN KEY (artist) REFERENCES artist(artist_id)
 );
 
 -- album(album_id, album_title, year, artist)
 CREATE TABLE album (
-    album_id INT PRIMARY KEY,
+    album_id VARCHAR(255) PRIMARY KEY,
     album_title VARCHAR(255),
     year INT,
-    artist INT,
+    artist VARCHAR(255),
     FOREIGN KEY (artist) REFERENCES artist(artist_id)
 );
 
 -- album_has(album_id, song_id)
 CREATE TABLE album_has (
-    album_id INT,
-    song_id INT,
+    album_id VARCHAR(255),
+    song_id VARCHAR(255),
     FOREIGN KEY (album_id) REFERENCES album(album_id),
     FOREIGN KEY (song_id) REFERENCES song(song_id)
 );
 
 -- artist(artist_id, artist_name, spotify_followers)
 CREATE TABLE artist (
-    artist_id INT PRIMARY KEY,
+    artist_id INT VARCHAR(255),
     artist_name VARCHAR(255),
     spotify_followers INT
 );
 
 -- releases(artist_id, release_id)
 CREATE TABLE releases (
-    artist_id INT,
-    release_id INT PRIMARY KEY,
+    artist_id VARCHAR(255),
+    release_id VARCHAR(255) PRIMARY KEY,
     FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
 
@@ -102,33 +103,33 @@ CREATE TABLE playlist (
     owner_username VARCHAR(255),
     date_created DATE,
     time_length INT,
-    song_titles VARCHAR(255),
+    -- song_titles VARCHAR(255),
     FOREIGN KEY (owner_username) REFERENCES user(username)
 );
 
 -- playlist_songs(playlist_id, song_id)
 CREATE TABLE playlist_songs (
     playlist_id INT,
-    song_id INT,
+    song_id VARCHAR(255),
     FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id),
     FOREIGN KEY (song_id) REFERENCES song(song_id)
 );
 
 -- library(username, playlists, artists, liked_songs, saved_albums)
-CREATE TABLE library (
-    username VARCHAR(255),
-    playlists VARCHAR(255),
-    artists VARCHAR(255),
-    liked_songs VARCHAR(255),
-    saved_albums VARCHAR(255),
-    FOREIGN KEY (username) REFERENCES user(username)
-);
+-- CREATE TABLE library (
+--     username VARCHAR(255),
+--     playlists VARCHAR(255),
+--     artists VARCHAR(255),
+--     liked_songs VARCHAR(255),
+--     saved_albums VARCHAR(255),
+--     FOREIGN KEY (username) REFERENCES user(username)
+-- );
 
 -- library_playlists(username, playlist_id)
 CREATE TABLE library_playlists (
     username VARCHAR(255),
     playlist_id INT,
-    FOREIGN KEY (username) REFERENCES library(username),
+    FOREIGN KEY (username) REFERENCES user(username),
     FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id)
 );
 
@@ -136,7 +137,7 @@ CREATE TABLE library_playlists (
 CREATE TABLE library_artists (
     username VARCHAR(255),
     artist_id INT,
-    FOREIGN KEY (username) REFERENCES library(username),
+    FOREIGN KEY (username) REFERENCES user(username),
     FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
 
@@ -144,7 +145,7 @@ CREATE TABLE library_artists (
 CREATE TABLE library_liked_songs (
     username VARCHAR(255),
     song_id INT,
-    FOREIGN KEY (username) REFERENCES library(username),
+    FOREIGN KEY (username) REFERENCES user(username),
     FOREIGN KEY (song_id) REFERENCES song(song_id)
 );
 
@@ -152,6 +153,6 @@ CREATE TABLE library_liked_songs (
 CREATE TABLE library_saved_albums (
     username VARCHAR(255),
     album_id INT,
-    FOREIGN KEY (username) REFERENCES library(username),
+    FOREIGN KEY (username) REFERENCES user(username),
     FOREIGN KEY (album_id) REFERENCES album(album_id)
 );

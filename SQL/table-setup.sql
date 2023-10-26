a21a014f5689645c27d791bd5ae910fb4d86c7ca
zangston <winston.y.zhang@gmail.com>
2023-10-26 16:08:57 -0400

    Create table-setup.sql

---
+++ SQL/table-setup.sql
@@ -0,0 +1,3 @@
+-- Running this script will populate the database with all of the tables needed for the app to work
+    -- Tables are created according to schema statements with were derived from E-R diagram and normalization
+

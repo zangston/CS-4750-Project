e9b9e4023ad98d8f5ab02181dc0dabe86296a754
gdw5bv <98567195+gdw5bv@users.noreply.github.com>
2023-10-27 01:57:14 -0400

    advanced sql commands

---
+++ SQL/advanced_commands.sql
@@ -0,0 +1,52 @@
+-- Advanced SQL commands
+-- Including CHECK Constraints and Triggers
+
+
+-- CHECK Constraints
+
+-- prevents users from registering with a username that already exists
+ALTER TABLE user
+ADD CONSTRAINT checkUsername UNIQUE (username);
+
+-- requires user passwords to have at least one capital letter and at least one number
+ALTER TABLE user
+ADD CONSTRAINT checkPassword
+CHECK (password LIKE '%[A-Z]%' AND password LIKE '%[0-9]%');
+
+-- checks that artist ID and release ID conform to Spotify standards (22 characters long)
+ALTER TABLE releases
+ADD CONSTRAINT checkIdLength
+CHECK (LENGTH(artist_id) == 22 AND LENGTH(release_id) == 22);
+
+-- prevents users from following an artist they already follow
+ALTER TABLE follows
+ADD CONSTRAINT checkDuplicateFollow UNIQUE (username, artist_id);
+
+
+-- Triggers
+
+-- update time_length on each created playlist when a user adds a song to that playlist
+DELIMITER $$
+CREATE OR REPLACE TRIGGER addSongTrigger
+BEFORE INSERT ON playlist_songs
+FOR EACH ROW
+BEGIN
+    UPDATE playlist
+    SET time_length = time_length + (SELECT duration FROM song WHERE song_id = new.song_id)
+    WHERE playlist_id = new.playlist_id
+END
+$$
+DELIMITER;
+
+-- update time_length on each created playlist when a user removes a song from that playlist
+DELIMITER $$
+CREATE OR REPLACE TRIGGER removeSongTrigger
+AFTER DELETE ON playlist_songs
+FOR EACH ROW
+BEGIN
+    UPDATE playlist
+    SET time_length = time_length - (SELECT duration FROM song WHERE song_id = new.song_id)
+    WHERE playlist_id = new.playlist_id
+END
+$$
+DELIMITER;

9f5e568629c2f0bb567736a8f3da2ecb3139b5e4
zangston <winston.y.zhang@gmail.com>
2023-11-29 15:20:25 -0500

    Resize terminal window to browser
    Very barebones bandaid solution

--- echoshell/terminal-init.js
+++ echoshell/terminal-init.js
@@ -1,3 +1,10 @@
 var term = new Terminal();
 term.open(document.getElementById('terminal'));
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+term.resize(208, 55);
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+term.onKey(e => {
+    console.log(e.key);
+    term.write(e.key);
+    if (e.key == '\r')
+        term.write('\n');
+})

cd395594081e4d29ad4a1dc65801db1d7ce3307b
zangston <winston.y.zhang@gmail.com>
2023-11-29 15:34:32 -0500

    Resize terminal to have margin
    No longer need to worry about fullscreen!

--- echoshell/terminal/terminal-init.js
+++ echoshell/terminal/terminal-init.js
@@ -1,6 +1,6 @@
 var term = new Terminal();
 term.open(document.getElementById('terminal'));
-term.resize(208, 55);
+term.resize(200, 40);
 term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
 term.onKey(e => {
     console.log(e.key);

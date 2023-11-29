e06caecc4ce6f6e0581df9230ecbc509636a9266
zangston <winston.y.zhang@gmail.com>
2023-11-29 15:58:56 -0500

    Comment code

--- echoshell/terminal/terminal-main.js
+++ echoshell/terminal/terminal-main.js
@@ -2,26 +2,30 @@
 var xterm = new Terminal();
 xterm.open(document.getElementById('terminal'));
 xterm.resize(200, 40);
-//xterm.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
-xterm.write("\x1B[0;32m")
+xterm.write("\x1B[0;32m")   // Note: colors dictated by ANSI scheme: https://bixense.com/clicolors/ 
 xterm.write("placeholder output - welcome to echoshell \r\nEchoshell $ ")
 
 // Terminal responses to keyboard inputs
 var currLine = "";
 var entries = [];
 xterm.onKey((ev) => {
+    // Enter: create new line
     if (ev.domEvent.key == "Enter") {
         if (currLine) {
             entries.push(currLine);
             xterm.write("\r\nEchoshell $ ");
             //Send cmd to backend here!
         }
-    } else if (ev.domEvent.key == "Backspace") {
+    } 
+    // Backspace: move cursor backward, erase character
+    else if (ev.domEvent.key == "Backspace") {
         if (currLine) {
             currLine = currLine.slice(0, currLine.length - 1);
             xterm.write("\b \b");
         }
-    } else {
+    } 
+    // Normal input: write character to terminal line
+    else {
         currLine += ev.key
         xterm.write(ev.key);
     }

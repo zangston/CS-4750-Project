d9872567b83f00bd44a9a5a2ad90b9ae87d40aea
unknown <ajs5qgm@virginia.edu>
2023-12-02 12:24:50 -0500

    everything but push to table for signup

--- echoshell/connect-db.php
+++ echoshell/connect-db.php
@@ -85,7 +85,7 @@ try
    $db = new PDO($dsn, $username, $password);
 
    // dispaly a message to let us know that we are connected to the database
-   // echo "<p>You are connected to the database: $dsn</p>"; // comment this out
+   echo "<p>You are connected to the database: $dsn</p>"; // comment this out
 }
 catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
 {

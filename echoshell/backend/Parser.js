a9f32ed401ab359753bddb232e06ab17f136721e
unknown <ajs5qgm@virginia.edu>
2023-12-02 11:07:04 -0500

    everything but push to table for signup

--- echoshell/backend/Parser.js
+++ echoshell/backend/Parser.js
@@ -1,53 +1,48 @@
 class Parser {
-
     loggedIn = false;
 
-    constructor()
-    {
-        var loggedIn = false;
+    constructor() {
+        this.loggedIn = false;
     }
 
     parseInput(input) {
-
         var tokens = input.split(' ');
-        var response = tokens[0] + " is not recognized as a command";
+        var response = "Command not recognized";
 
-        if(tokens[0].toLowerCase() == 'help')
-        {
-            response = "\nhelp\r\nlogin\r\nsignup\r\n";
-        }
-
-        if(tokens[0].toLowerCase() == 'login')
-        {   
-            if (this.loggedIn == false)
-            {
-                //TODO replace this with actual login code
-                response = "login request recieved";
-                this.loggedIn = true;
-            }
-            else
-                response = "already logged in!"
-        }
-
-        if(tokens[0].toLowerCase() == 'signup')
-        {   
-            if (this.loggedIn == false)
-            {
-                if(tokens[1].toLowerCase() == '-u' && tokens[3].toLowerCase() == '-p' && tokens.length() == 5){
-                    var user = tokens[2];
-                    var pswd = tokens[4];
-                    //TODO: Push tuple to database
-                    response = "signup successful!";
+        switch(tokens[0].toLowerCase()) {
+            case 'help':
+                response = "\r\nhelp\r\nlogin\r\nsignup\r\nlogout\n";
+                break;
+            case 'login':
+                if (!this.loggedIn) {
+                    // TODO replace this with actual login code
+                    response = "login request received";
                     this.loggedIn = true;
+                } else {
+                    response = "already logged in!";
                 }
-                else {
-                    response = "signup format incorrect: aborting signup.";
+                break;
+            case 'logout':
+                this.loggedIn = false;
+                response = "logout complete.";
+                break;
+            case 'signup':
+                if (!this.loggedIn) {
+                    if(tokens[1] && tokens[1].toLowerCase() == '-u' && tokens[3] && tokens[3].toLowerCase() == '-p' && tokens.length == 5) {
+                        var user = tokens[2];
+                        var pswd = tokens[4];
+                        // TODO: Push tuple to database
+                        response = "signup successful!";
+                        this.loggedIn = true;
+                    } else {
+                        response = "signup format incorrect: aborting signup.";
+                    }
+                } else {
+                    response = "already logged in!";
                 }
-            }
-            else
-                response = "already logged in!"
+                break;
         }
-        
+
         return response;
     }
 }

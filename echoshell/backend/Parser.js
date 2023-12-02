551c9c81d2e30bfe98157de5c5260fe0c6ba41fe
zangston <winston.y.zhang@gmail.com>
2023-12-02 18:28:31 -0500

    Refactor parser

--- echoshell/backend/Parser.js
+++ echoshell/backend/Parser.js
@@ -7,13 +7,14 @@ class Parser {
 
     parseInput(input) {
         var tokens = input.split(' ');
-        var response = tokens[0] + " is not recognized as a command";
+        var command = tokens[0]
+        var response = command + " is not recognized as a command";
 
-        if (tokens[0].toLowerCase() == 'help') {
+        if (command.toLowerCase() == 'help') {
             response = "\nhelp\r\nlogin\r\nsignup\r\nlogout";
         }
 
-        if (tokens[0].toLowerCase() == 'login') {
+        if (command.toLowerCase() == 'login') {
             if (!this.loggedIn) {
                 //TODO replace this with actual login code
                 response = "login request received";
@@ -23,12 +24,12 @@ class Parser {
             }
         }
 
-        if (tokens[0].toLowerCase() == 'logout') {
+        if (command.toLowerCase() == 'logout') {
             this.loggedIn = false;
             response = "logout complete.";
         }
 
-        if (tokens[0].toLowerCase() == 'signup') {
+        if (command.toLowerCase() == 'signup') {
             if (!this.loggedIn) {
                 if (tokens[1] && tokens[1].toLowerCase() == '-u' && tokens[3] && tokens[3].toLowerCase() == '-p' && tokens.length == 5) {
                     var user = tokens[2];

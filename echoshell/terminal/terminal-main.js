2c590dd7a74c88b79a50e0556f151bd0d50802c1
zangston <winston.y.zhang@gmail.com>
2023-12-03 13:41:31 -0500

    Add up-down arrow key command history

--- echoshell/terminal/terminal-main.js
+++ echoshell/terminal/terminal-main.js
@@ -9,30 +9,61 @@ xterm.write("Echoshell $ ");
 // Terminal responses to keyboard inputs
 var currLine = "";
 var entries = [];
-xterm.onKey(async (ev) => {
-    // Enter: create new line
-    if (ev.domEvent.key == "Enter") {
-        if (currLine) {
-            entries.push(currLine);
-            xterm.write("\r\n");
+var historyIndex = 0;
+var maxHistoryIndex;
 
-            //Send cmd to backend parser
-            response = await p.parseInput(currLine);
-            
-            currLine = "";
-            xterm.write(response + "\r\nEchoshell $ ");
-        }
+xterm.onKey(async (ev) => {
+  // Enter: create new line
+  if (ev.domEvent.key === "Enter") {
+    if (currLine) {
+      entries.push(currLine);
+      historyIndex = entries.length; // Move history index to the end
+      xterm.write("\r\n");
+
+      // Send cmd to backend parser
+      const response = await p.parseInput(currLine);
+
+      currLine = "";
+      xterm.write(response + "\r\nEchoshell $ ");
     }
-    // Backspace: move cursor backward, erase character
-    else if (ev.domEvent.key == "Backspace") {
-        if (currLine) {
-            currLine = currLine.slice(0, currLine.length - 1);
-            xterm.write("\b \b");
-        }
+  }
+  // Backspace: move cursor backward, erase character
+  else if (ev.domEvent.key === "Backspace") {
+    if (currLine) {
+      currLine = currLine.slice(0, currLine.length - 1);
+      xterm.write("\b \b");
     }
-    // Normal input: write character to terminal line
-    else {
-        currLine += ev.key
-        xterm.write(ev.key);
+  }
+  // Arrow Up: retrieve previous command from history
+  else if (ev.domEvent.key === "ArrowUp") {
+    if (historyIndex > 0) {
+      historyIndex--;
+      eraseLine();
+      currLine = entries[historyIndex];
+      xterm.write('Echoshell $ ' + currLine);
     }
+  }
+  // Arrow Down: retrieve next command from history
+  else if (ev.domEvent.key === "ArrowDown") {
+    if (historyIndex < entries.length - 1) {
+      historyIndex++;
+      eraseLine();
+      currLine = entries[historyIndex];
+      xterm.write('Echoshell $ ' + currLine);
+    } else {
+      // If at the end of history, clear the line
+      eraseLine();
+      currLine = "";
+      xterm.write('Echoshell $ ' + currLine);
+    }
+  }
+  // Normal input: write character to terminal line
+  else {
+    currLine += ev.key;
+    xterm.write(ev.key);
+  }
 });
+
+function eraseLine() {
+  xterm.write("\r\x1b[K");
+}
